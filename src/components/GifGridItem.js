f3b991922319fd9cbdc7bf95f9ecc68f27bf3c96
Carlos Martínez Crespo <carlos.martinez.crespo@gmail.com>
2020-08-05 18:19:13 +0200

    refactor: added propTypes in GifGridItem component

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -2,20 +2,29 @@ import 'primereact/resources/themes/nova-dark/theme.css';
 import 'primereact/resources/primereact.css';
 
 import React from 'react'
+import PropTypes from 'prop-types';
 import { Card } from 'primereact/card';
 
-export const GifGridItem = ({ id, title, url}) => {
+export const GifGridItem = ({ title, url}) => {
     
-    const header = (
-        <img src={ url } alt={ title }/>
-    );
+    const header = ( <img src={ url } alt={ title }/> );
 
     return (
         <Card
             subTitle={ title }
             style={{width: '360px'}}
             className="ui-card-shadow card animate__animated animate__fadeIn"
-            header={header}
+            header={ header }
         />
     )
+}
+
+GifGridItem.propTypes = {
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+}
+
+Card.propTypes = {
+    subTitle: PropTypes.string.isRequired,
+    header: PropTypes.object.isRequired
 }
